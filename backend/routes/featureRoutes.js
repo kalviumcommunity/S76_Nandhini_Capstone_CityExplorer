d2f8154dc45c08f3367fb85301693c87f08b4830
Nandhini Murugan <nandz1407@gmail.com>
2025-05-16 17:09:42 +0530

    Added new files and updates to routes/models

--- backend/routes/featureRoutes.js
+++ backend/routes/featureRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Feature = require('../models/Feature');
 
+
 // @desc    Get all features
 // @route   GET /api/features
 // @access  Public
